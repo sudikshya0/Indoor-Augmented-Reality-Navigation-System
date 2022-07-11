750c7259453d29d67b97fa42b5bca13e5ce55ee8
sudikshya0 <sudikshya.rajkarnikar22@gmail.com>
2022-07-11 21:35:41 +0545

    commit

---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject startbutton;
+
+    void Onclick()
+    {
+        
+
+        
+            this.gameObject.SetActive(false);
+      
+    }
+
+   
+}
